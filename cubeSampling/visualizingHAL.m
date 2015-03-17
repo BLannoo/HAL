184d2caec81365a64ea1768e8295921adb0edda4
BLannoo <brunolannoo@gmail.com>
2015-03-17 15:39:04 +0100

    found more starting points

--- cubeSampling/visualizingHAL.m
+++ cubeSampling/visualizingHAL.m
@@ -9,24 +9,35 @@ clc
 N = 1000; % Number of data points (~2sec per data point)
 scale = 1.1;
 seed = 2;
-dataSet = 2;
 
-%     th   dm    km     ka   kb     kab  al   kma    da     db   dab  diss
-k = [[0.1, 0.06, 0.007, 50,  4,     100, 0.1, 0.001, 0.001, 0.001, 1, 0.01];
-     [1,   0.36, 100,   100, 25459, 100, 1,   2,     0.08,  0.28,  1, 0.01]];
-
-filename = sprintf('N%i_scale%.1f_dataSet%i.mat',N,scale,dataSet);
-
-kInitial = k(dataSet,:);
+%     th    dm     km     ka   kb     kab  al    kma    da     db     dab  diss
+k = [[0.1,  0.06,  0.007, 50,  4,     100, 0.1,  0.001, 0.001, 0.001, 1,   0.01];
+     [1,    0.36,  100,   100, 25459, 100, 1,    2,     0.08,  0.28,  1,   0.01];
+     [0.1,  0.11,  48,    8.5, 1890,  100, 0.01, 5,     2.1,   0.92,  100, 100];
+     [0.05, 0.33,  37,    1.4, 40,    100, 0.32, 0.9,   2.7,   0.04,  2.8, 14];
+     [0.14, 0.4,   100,   0.9, 48,    4.9, 0.05, 3.3,   0.08,  0.16,  100, 100];
+     [0.06, 0.13,  7.4,   1.9, 54,    17,  0.01, 1.8,   0.07,  0.26,  100, 100];
+     [0.01, 0.1,   7.2,   0.2, 4.6,   8.8, 0.1,  0.22,  0.34,  0.02,  100, 100]; ... seed 139999
+     [0.08, 0.04,  23,    0.5, 200,   0.8, 0.01, 7.27,  0.03,  0.02,  100, 100]; ... seed 130001
+     [1.03, 0.037, 17,    13,  5980,  95,  0.01, 5.3,   .063,  15.7,  100, 13.1]; ... seed 130005
+     ];
 
 %% Get data
 
-if exist(filename,'file')
-    load(filename)
-else
-    tic
-    [params,Aperiod,Bperiod,numPeaks,Amax,Amin,Bmax,Bmin,beta,numWarning] = samplingHAL(N,kInitial,scale,seed,filename);
-    toc
+for dataSet = 1:size(k,1);
+
+    filename = sprintf('N%i_scale%.1f_dataSet%i.mat',N,scale,dataSet);
+
+    kInitial = k(dataSet,:);
+
+    if exist(filename,'file')
+        load(filename)
+    else
+        tic
+        [params,Aperiod,Bperiod,numPeaks,Amax,Amin,Bmax,Bmin,beta,numWarning] = samplingHAL(N,kInitial,scale,seed,filename);
+        toc
+    end
+
 end
 
 %% sampling
