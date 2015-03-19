a1e26b78fe65a4e176e659462e6b521e861bf951
BLannoo <brunolannoo@gmail.com>
2015-03-19 18:02:33 +0100

    found biologically realistic case close to analytic case

--- cubeSampling/visualizingHAL.m
+++ cubeSampling/visualizingHAL.m
@@ -19,12 +19,26 @@ k = [[0.1,  0.06,  0.007, 50,  4,     100, 0.1,  0.001, 0.001, 0.001, 1,   0.01]
      [0.06, 0.13,  7.4,   1.9, 54,    17,  0.01, 1.8,   0.07,  0.26,  100, 100];
      [0.01, 0.1,   7.2,   0.2, 4.6,   8.8, 0.1,  0.22,  0.34,  0.02,  100, 100]; ... seed 139999
      [0.08, 0.04,  23,    0.5, 200,   0.8, 0.01, 7.27,  0.03,  0.02,  100, 100]; ... seed 130001
-     [0.0001, 0.1, 2, 1000, 25, 100000, 1000, 0.0, 0.0, 0.0, 10000, 0.0]; ... analytic limit
+     [0.0001, 0.1, 2,    1000, 25, 100000, 1000, 0.0,   0.0,   0.0, 10000, 0.0]; ... analytic limit
+     [0.05, 0.06,  2,     50,  100,   100, 0.1,  0,     0,     0,     1,   0]; ... analytic limit (close to biology)
+     [0.05, 0.2,   2,     50,  100,   100, 0.05, 0,     0,     0,     1,   0]; ... analytic limit (close to biology)
+     [0.01, 0.1,   2,     50,  25,    100, 0.05, 0,     0,     0,     1,   0]; ... analytic limit (close to biology)
+     [0.01, 0.1,   2,     50,  50,   1000, 0.05, 0,     0,     0,     1,   0]; ... analytic limit (close to biology)
+     [0.01, 0.05,  2,     50,  100,  1000, 0.05, 0,     0,     0,     1,   0]; ... analytic limit (close to biology)
+     [0.01, 0.05,  2,     50,  100,  1000, 0.05, 0,     0,     0,     1,   0]; ... analytic limit (close to biology)
+     [0.01, 0.05,  2,     25,  100,  1000, 0.05, 0,     0,     0,     1,   0]; ... analytic limit (close to biology)
+     [0.01, 0.075, 2,     25,  100,  1000, 0.05, 0,     0,     0,     1,   0]; ... analytic limit (close to biology)
+     [0.01, 0.075, 2,     25,  100,  1000, 0.01, 0,     0,     0,     1,   0]; ... analytic limit (close to biology)
+     [0.01, 0.05,  2,     25,  100,  1000, 0.01, 0,     0,     0,     1,   0]; ... analytic limit (close to biology)
+     [0.01, 0.05,  2,     25,  100,  1000, 0.01, 0.001, 0.001, 0.001, 1,   0.001]; ... analytic limit (close to biology)
      ];
 
 %% Get data
 
-for dataSet = 1:9;
+dataSets = size(k,1)-8:size(k,1);
+
+for i2 = 1:9
+    dataSet = dataSets(i2);
 
 filename = sprintf('N%i_scale%.1f_dataSet%i.mat',N,scale,dataSet);
 
@@ -58,21 +72,21 @@ end
 
 %% Plot phase lengths
 
-[betaAnalytic,TAanalytic,TBanalytic] = analyticHAL(0.5:0.1:10);
+[betaAnalytic,TAanalytic,TBanalytic] = analyticHAL(0.5:0.1:20);
 
-figure(1)
-subplot(3,3,dataSet)
+figure(i2)
+%subplot(3,3,i2)
 hold all
 
-semilogx(betaAnalytic,TAanalytic,'b-','linewidth',5)
-semilogx(betaAnalytic,TBanalytic,'g-','linewidth',5)
-semilogx(betaAnalytic,TAanalytic+TBanalytic,'k-','linewidth',5)
+semilogx(betaAnalytic,TAanalytic,'b-','linewidth',3)
+semilogx(betaAnalytic,TBanalytic,'g-','linewidth',3)
+semilogx(betaAnalytic,TAanalytic+TBanalytic,'k-','linewidth',3)
 
 semilogx(beta(numPeaks>9),Aperiod(numPeaks>9).*params(numPeaks>9,2)','b.')
 semilogx(beta(numPeaks>9),Bperiod(numPeaks>9).*params(numPeaks>9,2)','g.')
 semilogx(beta(numPeaks>9),(Aperiod(numPeaks>9)+Bperiod(numPeaks>9)).*params(numPeaks>9,2)','k.')
 
-axis([0 5 0 10])
+axis([0 10 0 20])
 
 %legend('length A phase','length B phase','period','length A phase','length B phase','period')
 
@@ -81,7 +95,7 @@ xlabel('beta')
 ylabel('period')
 
 set(findall(gcf,'-property','FontSize'),'FontSize',25)
-set(findall(gcf,'-property','MarkerSize'),'MarkerSize',25)
+set(findall(gcf,'-property','MarkerSize'),'MarkerSize',15)
 
 %% Plot Amplitude distributions
 
@@ -101,27 +115,27 @@ set(findall(gcf,'-property','MarkerSize'),'MarkerSize',25)
 
 %% Plot coefficient of variation
 
-CV = @(x) std(x(numPeaks>9))./mean(x(numPeaks>9));
-
-CV_Aperiod = CV(Aperiod);
-CV_Bperiod = CV(Bperiod);
-CV_period = CV(Aperiod+Bperiod);
-CV_beta = CV(beta);
-
-CV_par = [];
-for i1 = 1:12
-    CV_par = [CV_par CV(params(:,i1))];
-end
-
-figure(3)
-subplot(3,3,dataSet)
-boxplot([CV_Aperiod+zeros(size(CV_par));CV_Bperiod+zeros(size(CV_par));...
-    CV_period+zeros(size(CV_par));CV_par;CV_beta+zeros(size(CV_par))]' ...
-    ... ,'labels',{'length A phase','length B phase','period','params','beta'} ...
-    )
-
-ylabel('CV')
-
-set(findall(gcf,'-property','FontSize'),'FontSize',25)
+% CV = @(x) std(x(numPeaks>9))./mean(x(numPeaks>9));
+% 
+% CV_Aperiod = CV(Aperiod);
+% CV_Bperiod = CV(Bperiod);
+% CV_period = CV(Aperiod+Bperiod);
+% CV_beta = CV(beta);
+% 
+% CV_par = [];
+% for i1 = 1:12
+%     CV_par = [CV_par CV(params(:,i1))];
+% end
+% 
+% figure(3)
+% %subplot(3,3,i2)
+% boxplot([CV_Aperiod+zeros(size(CV_par));CV_Bperiod+zeros(size(CV_par));...
+%     CV_period+zeros(size(CV_par));CV_par;CV_beta+zeros(size(CV_par))]' ...
+%     ... ,'labels',{'length A phase','length B phase','period','params','beta'} ...
+%     )
+% 
+% ylabel('CV')
+% 
+% set(findall(gcf,'-property','FontSize'),'FontSize',25)
 
 end
